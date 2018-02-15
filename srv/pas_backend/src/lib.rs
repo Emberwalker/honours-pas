91a761862615b9d04a7bf06cc8e7f4a8672b817f
Arkan <arkan@drakon.io>
2018-02-15 11:27:08 +0000

    Commit skeleton of Rust backend

---
+++ srv/pas_backend/src/lib.rs
@@ -0,0 +1,7 @@
+extern crate futures;
+extern crate tokio_core;
+#[macro_use]
+extern crate hyper;
+#[macro_use]
+extern crate serde_derive;
+extern crate serde_json;
---
+++ srv/pas_backend/src/main.rs
@@ -0,0 +1,5 @@
+extern crate pas_backend;
+
+fn main() {
+	
+}
