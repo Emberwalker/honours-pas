b1a5a032d05d7556ddfc2824ea03de40df261c05
Arkan <arkan@drakon.io>
2018-02-21 14:13:04 +0000

    Add way to add Rocket routes for auth backends.

--- srv/src/authn/mod.rs
+++ srv/src/authn/mod.rs
@@ -1,3 +1,5 @@
+use rocket::Route;
+
 use db::Pool;
 
 pub trait AuthnBackend<'a> {
@@ -5,7 +7,14 @@ pub trait AuthnBackend<'a> {
     /// for saving a reference to the database pool (if required). Making use of Serde and toml-rs is recommended.
     fn init(config_location: &str, pool: &'a Pool);
 
+    /// Provides a set of Rocket routes. These will be mounted at "/api/authn", for tasks such as e.g. email
+    /// verification endpoints. On success, ideally redirect back to "/".If no routes are required, return an empty vec.
+    /// When generating the vector, it's probably best to use the Rocket `routes![]` macro.
+    fn get_rocket_routes() -> Vec<Route> {
+        Vec::new()
+    }
+
     /// Called when the system attempts to authenticate a user. Returns the users externally-visible email address if
-    /// authentication succeeds.
+    /// authentication succeeds, else None.
     fn authenticate(username: &str, password: &str) -> Option<String>;
 }
